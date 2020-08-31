47b0a3f3e7265bd89f8f7112da82f4ca2e4997d3
Mircea-Adrian Casapu <mircea.casapu@student.upt.ro>
2020-08-31 17:12:02 +0300

    Fixed a few content issues

---
+++ Scripts/index.js
@@ -0,0 +1,6 @@
+var MoreTextBttn = document.getElementById("show-more-button")
+console.log(MoreTextBttn)
+
+// document.getElementsByClassName("show-more-button").addEventListener('click', function() {
+//     console.log("TEst");
+//  }, false);
