bd413e8a3c5e76d292f48558dc3e75a745e37fc3
Mircea-Adrian Casapu <mircea.casapu@student.upt.ro>
2020-08-31 17:47:51 +0300

    Added "Mai mult/Mai putin" buttons

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,6 +1,19 @@
-var MoreTextBttn = document.getElementById("show-more-button")
-console.log(MoreTextBttn)
+var moreButtons = document.getElementsByClassName("show-more-button");
+var moreText = document.getElementsByClassName("show-more-text");
 
-// document.getElementsByClassName("show-more-button").addEventListener('click', function() {
-//     console.log("TEst");
-//  }, false);
+for(let i=0;i<moreButtons.length;i++)
+{
+    moreButtons[i].addEventListener('click', function() {
+        if(moreButtons[i].innerHTML === 'Mai mult')
+        {
+            moreButtons[i].innerHTML = 'Mai putin';
+            moreText[i].classList.remove("invisible-text");
+            moreText[i].classList.add("visible-text");
+        }
+        else {
+            moreButtons[i].innerHTML = 'Mai mult';
+            moreText[i].classList.remove("visible-text");
+            moreText[i].classList.add("invisible-text");
+        }
+    })
+}
