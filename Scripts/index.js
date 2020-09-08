7b36473f170735b709c83344692b6f37703d78d6
Mircea Casapu <mircea.casapu@gmail.com>
2020-09-08 21:58:02 +0300

    added jquery for styling focused nav links

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,10 +1,18 @@
 $(document).ready(function () {
   var moreButtons = document.getElementsByClassName("show-more-button");
   var moreText = document.getElementsByClassName("show-more-text");
+  var mobileNavLinks = document.getElementsByClassName("nav-mobile-link")
 
   for (let i = 0; i < moreButtons.length; i++) {
     $(moreButtons[i]).click(function() {
       $(moreText[i]).slideToggle(200);
+    })
+  }
+
+  for(let i=0; i<=mobileNavLinks.length; i++) {
+    $(mobileNavLinks[i]).click(function () {
+      $(mobileNavLinks).removeClass("nav-mobile-active")
+      $(mobileNavLinks[i]).addClass("nav-mobile-active")
     })
   }
 })
